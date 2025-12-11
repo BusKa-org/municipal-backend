a91c39dc48b5899ba99bdc5f079d36cf5937428d
mttgermano <mttgermano@proton.me>
2025-12-11 17:08:22 -0300

    fix: import uuid

--- app/models/user.py
+++ app/models/user.py
@@ -3,6 +3,7 @@ from sqlalchemy import Enum as SQLEnum
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 from enum import Enum
+import uuid
 
 class UserRole(str, Enum):
     ALUNO = "aluno"
