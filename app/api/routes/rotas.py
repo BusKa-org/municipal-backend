282b37c5fb8ec31fecf437d0982566be494c0a1c
mttgermano <mttgermano@proton.me>
2025-12-12 01:56:13 -0300

    fix: inconsistent function names @routes/rotas

--- app/api/routes/rotas.py
+++ app/api/routes/rotas.py
@@ -11,25 +11,23 @@ docs_prefix = "../../../../../docs/endpoints/"
 @rotas_bp.route("/", methods=["GET"])
 @swag_from(docs_prefix + "motorista-listar_rotas.yml")
 @jwt_required()
-def listar_rotas():
+def list_all_rotas():
     return RotasController.list_all_rotas()
-
 
 @rotas_bp.route("/", methods=["POST"])
 @swag_from(docs_prefix + "motorista-criar_rota.yml")
 @jwt_required()
-def criar_rota():
+def create_rota():
     return RotasController.create_rota()
-
 
 @rotas_bp.route("/<int:rota_id>/ponto", methods=["POST"])
 @swag_from(docs_prefix + "motorista-adicionar_ponto.yml")
 @jwt_required()
-def adicionar_ponto(rota_id):
+def add_ponto(rota_id):
     return RotasController.add_ponto(rota_id)
 
 @rotas_bp.route("/<int:rota_id>/inscricao", methods=["PUT"])
 @swag_from(docs_prefix+'aluno-inscricao_rota.yml')
 @jwt_required()
-def adicionar_ponto(rota_id):
+def inscricao_aluno_rota(rota_id):
     return RotasController.inscricao_aluno_rota(rota_id)
