3586870d6c1eb57b27655e84debf8d9a1219d61d
mttgermano <mttgermano@proton.me>
2025-12-12 01:32:35 -0300

    update: add new routes and deleted old ones @routes/__init__.py

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -4,12 +4,12 @@ def register_routes(app):
     """Registers all route blueprints."""
     from .auth import auth_bp
     from .user import user_bp
-    from .aluno import aluno_bp
-    from .motorista import motorista_bp
-    from .gestor import gestor_bp
+    from .me import self_bp
+    from .rotas import rotas_bp
+    from .viagens import viagens_bp
 
     app.register_blueprint(auth_bp, url_prefix="/auth")
-    app.register_blueprint(aluno_bp, url_prefix="/aluno")
     app.register_blueprint(user_bp, url_prefix="/user")
-    app.register_blueprint(motorista_bp, url_prefix="/motorista")
-    app.register_blueprint(gestor_bp, url_prefix="/gestor")
+    app.register_blueprint(self_bp, url_prefix="/me")
+    app.register_blueprint(self_bp, url_prefix="/rotas")
+    app.register_blueprint(self_bp, url_prefix="/viagens")
