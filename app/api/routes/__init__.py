9c012089edbcb8e4c87512799f3d20d888e928c9
mttgermano <mttgermano@proton.me>
2025-12-12 01:59:46 -0300

    fix: can't register a route named 'self', renamed to 'me'

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -4,12 +4,12 @@ def register_routes(app):
     """Registers all route blueprints."""
     from .auth import auth_bp
     from .user import user_bp
-    from .me import self_bp
+    from .me import me_bp
     from .rotas import rotas_bp
     from .viagens import viagens_bp
 
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(user_bp, url_prefix="/user")
-    app.register_blueprint(self_bp, url_prefix="/me")
+    app.register_blueprint(me_bp, url_prefix="/me")
     app.register_blueprint(self_bp, url_prefix="/rotas")
     app.register_blueprint(self_bp, url_prefix="/viagens")
--- app/api/routes/me.py
+++ app/api/routes/me.py
@@ -6,30 +6,30 @@ from ..controllers.user_controller import UserController
 from ..controllers.rotas_controller import RotasController
 from ..controllers.viagens_controller import ViagensController
 
-self_bp = Blueprint("self", __name__)
+me_bp = Blueprint("me", __name__)
 docs_prefix = "../../../../../docs/endpoints/"
 
 
-@self_bp.route("/", methods=["GET"])
+@me_bp.route("/", methods=["GET"])
 @swag_from(docs_prefix + 'user-me.yml')
 @jwt_required()
 def get_current_user():
     return UserController.get_current_user()
 
 
-@self_bp.route("/", methods=["PUT"])
+@me_bp.route("/", methods=["PUT"])
 @swag_from(docs_prefix + 'user-update.yml')
 @jwt_required()
 def update_user():
     return UserController.update_user()
 
-@self_bp.route("/rotas", methods=["GET"])
+@me_bp.route("/rotas", methods=["GET"])
 @swag_from(docs_prefix + "aluno-listar_rotas.yml")
 @jwt_required()
 def list_my_rotas():
     return RotasController.list_my_rotas()
 
-@self_bp.route("/viagens", methods=["GET"])
+@me_bp.route("/viagens", methods=["GET"])
 @swag_from(docs_prefix + "aluno-listar_viagens.yml")
 @jwt_required()
 def list_my_viagens():
