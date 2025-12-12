5927494778d72c0a8079e5ac51b46c6837c54396
mttgermano <mttgermano@proton.me>
2025-12-12 01:53:22 -0300

    fix: rotas_bp -> viagens_bp @routes/viagens

--- app/api/routes/viagens.py
+++ app/api/routes/viagens.py
@@ -4,30 +4,30 @@ from flasgger import swag_from
 
 from ..controllers.viagens_controller import ViagensController
 
-rotas_bp = Blueprint("viagens", __name__)
+viagens_bp = Blueprint("viagens", __name__)
 docs_prefix = "../../../../../docs/endpoints/"
 
 
-@rotas_bp.route("/", methods=["GET"])
+@viagens_bp.route("/", methods=["GET"])
 @swag_from(docs_prefix + "motorista-listar_viagens.yml")
 @jwt_required()
 def listar_viagens():
     return ViagensController.list_all_viagens()
 
-@rotas_bp.route("/<int:viagem_id>/inscricao", methods=["GET"])
+@viagens_bp.route("/<int:viagem_id>/inscricao", methods=["GET"])
 @swag_from(docs_prefix+'aluno-presenca_viagem.yml')
 @jwt_required()
 def inscricao_viagem(viagem_id):
     return ViagensController.inscricao_aluno_viagem(viagem_id)
 
-@rotas_bp.route("/<int:viagem_id>/iniciar", methods=["POST"])
+@viagens_bp.route("/<int:viagem_id>/iniciar", methods=["POST"])
 @swag_from(docs_prefix + "motorista-iniciar_viagem.yml")
 @jwt_required()
 def iniciar_viagem(viagem_id):
     return ViagensController.start_viagem(viagem_id)
 
 
-@rotas_bp.route("/<int:viagem_id>/finalizar", methods=["POST"])
+@viagens_bp.route("/<int:viagem_id>/finalizar", methods=["POST"])
 @swag_from(docs_prefix + "motorista-finalizar_viagem.yml")
 @jwt_required()
 def finalizar_viagem(viagem_id):
