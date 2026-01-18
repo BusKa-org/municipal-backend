94bb9118f6238ba6c73d115457eff24608775998
Wendell Tomé Marinho Oliveira <wendell.tome.marinho.oliveira@ccc.ufcg.edu.br>
2026-01-18 11:55:54 -0300

    refactor: adapting __init__.py to the new database schema

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,8 @@
-from .user import User, UserRole
-from .municipio import Municipio
-from .rota import Rota, Ponto, RotaAluno
-from .viagem import Viagem, ViagemAluno 
-from .notificacao import Notificacao
+# app/models/__init__.py
+from .user import User, Aluno, Motorista, Gestor
+from .geo import Ponto, Endereco, Instituicao
+from .frota import Onibus
+from .rota import Rota, RotaPonto, HorarioRota, DiasOperacao
+from .viagem import Viagem, ViagemPonto, AlunosConfirmados
+from .notificacao import Notificacao  # <--- FALTAVA ISSO
+from .enum import DiaDaSemana, SentidoViagem, StatusViagem
