462dbd35b7dc3979dc24b04de2f4235f786af809
Wendell Tomé Marinho Oliveira <wendell.tome.marinho.oliveira@ccc.ufcg.edu.br>
2026-01-18 14:42:00 -0300

    fix: outdated syntax use

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -4,22 +4,22 @@ class UserCreateSchema(Schema):
     nome = fields.String(required=True)
     email = fields.Email(required=True)
     password = fields.String(required=True, load_only=True)
-    telefone = fields.String(missing=None)
+    
+    telefone = fields.String(load_default=None)
     cpf = fields.String(required=True)
     
     role = fields.String(required=True, validate=validate.OneOf(["aluno", "motorista", "gestor"]))
     
-    matricula = fields.String(missing=None)
-    nome_pai = fields.String(missing=None)
-    nome_mae = fields.String(missing=None)
-    cnh = fields.String(missing=None)
-    salario = fields.Float(missing=None)
+    matricula = fields.String(load_default=None)
+    nome_pai = fields.String(load_default=None)
+    nome_mae = fields.String(load_default=None)
+    cnh = fields.String(load_default=None)
+    salario = fields.Float(load_default=None)
 
 class UserResponseSchema(Schema):
     id = fields.UUID(dump_only=True)
     nome = fields.String()
     email = fields.String()
-
     role = fields.Method("get_role")
 
     def get_role(self, obj):
