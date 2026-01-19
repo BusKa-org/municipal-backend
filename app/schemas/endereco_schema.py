b786063f0af35e14f69bfd9bc82a1ac6b519fa2b
Wendell Tomé Marinho Oliveira <wendell.tome.marinho.oliveira@ccc.ufcg.edu.br>
2026-01-19 15:58:13 -0300

    chore(address): creating its own schema

---
+++ app/schemas/endereco_schema.py
@@ -0,0 +1,10 @@
+from marshmallow import Schema, fields
+
+class EnderecoInputSchema(Schema):
+    logradouro = fields.String(required=True)
+    numero = fields.String(required=True)
+    bairro = fields.String(required=True)
+    cidade = fields.String(required=True)
+    cep = fields.String(required=True)
+    latitude = fields.Float(required=True)
+    longitude = fields.Float(required=True)
--- app/schemas/instituicao_schema.py
+++ app/schemas/instituicao_schema.py
@@ -1,13 +1,5 @@
 from marshmallow import Schema, fields, validate
-
-class EnderecoSchema(Schema):
-    logradouro = fields.String(required=True)
-    numero = fields.String(required=True)
-    bairro = fields.String(required=True)
-    cidade = fields.String(required=True)
-    cep = fields.String(required=True)
-    latitude = fields.Float(required=True, metadata={"description": "Latitude do local"})
-    longitude = fields.Float(required=True, metadata={"description": "Longitude do local"})
+from app.schemas.endereco_schema import EnderecoInputSchema
 
 class InstituicaoCreateSchema(Schema):
     nome = fields.String(required=True)
@@ -20,13 +12,13 @@ class InstituicaoCreateSchema(Schema):
         ]),
         metadata={"description": "Tipo da instituição conforme modelo"}
     )
-    endereco = fields.Nested(EnderecoSchema, required=True)
+    endereco = fields.Nested(EnderecoInputSchema, required=True)
 
 class InstituicaoResponseSchema(Schema):
     id = fields.String()
     nome = fields.String()
     cnpj = fields.String()
     tipo = fields.String()
-    endereco = fields.Nested(EnderecoSchema, attribute="ponto.endereco")
+    endereco = fields.Nested(EnderecoInputSchema, attribute="ponto.endereco")
     latitude = fields.Float(attribute="ponto.latitude")
     longitude = fields.Float(attribute="ponto.longitude")
