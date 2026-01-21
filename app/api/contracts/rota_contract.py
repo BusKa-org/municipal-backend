4cd82bac8ff641537a01c7c840d5e19141eb2b14
Wendell Tomé Marinho Oliveira <wendell.tome.marinho.oliveira@ccc.ufcg.edu.br>
2026-01-21 11:19:26 -0300

    fix(route): correcting the creation of the operational days with the creationg of the route

--- app/api/contracts/rota_contract.py
+++ app/api/contracts/rota_contract.py
@@ -2,17 +2,36 @@ from flask_restx import fields
 
 class RotaContract:
     @staticmethod
-    def create_model(api):
-        return api.model('RotaCreate', {
-            'nome': fields.String(required=True, example='Rota Universitária Manhã'),
-            'motorista_id': fields.String(description='UUID do Motorista Padrão'),
-            'veiculo_id': fields.String(description='UUID do Ônibus Padrão')
+    def ponto_model(api):
+        return api.model('PontoRotaInput', {
+            'latitude': fields.Float(required=True, example=-23.5505),
+            'longitude': fields.Float(required=True, example=-46.6333),
+            'ordem': fields.Integer(required=True, example=1),
+            'apelido': fields.String(description='Nome opcional', example="Ponto da Praça")
         })
 
     @staticmethod
+    def rota_input_model(api):
+        horario = RotaContract.horario_model(api)
+        ponto = RotaContract.ponto_model(api)
+
+        return api.model('RotaCreateFull', {
+            'nome': fields.String(required=True, description='Nome da Rota', example="Rota 101 - Centro"),
+            'motorista_padrao_id': fields.String(description='UUID do Motorista Padrão (Opcional)', example="uuid-do-motorista"),
+            'veiculo_padrao_id': fields.String(description='UUID do Veículo Padrão (Opcional)', example="uuid-do-onibus"),
+            
+            'pontos': fields.List(fields.Nested(ponto), description='Lista ordenada de pontos geográficos'),
+            'horarios': fields.List(fields.Nested(horario), description='Grade de horários e dias de operação')
+        })
+        
+            
+    @staticmethod
     def horario_model(api):
-        return api.model('HorarioCreate', {
-            'horario_saida': fields.String(required=True, example='07:30', description='Formato HH:MM'),
-            'sentido': fields.String(required=True, enum=['IDA', 'VOLTA', 'CIRCULAR']),
-            'dias': fields.List(fields.String(enum=['SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB', 'DOM']), required=True)
+        return api.model('HorarioInput', {
+            'horario_saida': fields.String(required=True, description='Horário de saída (HH:MM)', example="07:00"),
+            'sentido': fields.String(required=True, description='IDA ou VOLTA', enum=['IDA', 'VOLTA'], example="IDA"),
+            'dias': fields.List(fields.String, required=True, 
+                                description='Dias de operação', 
+                                enum=['SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB', 'DOM'],
+                                example=["SEG", "QUA", "SEX"])
         })
